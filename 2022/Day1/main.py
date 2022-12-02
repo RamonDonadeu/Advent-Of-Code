c6b897d32cec2cbf8b9be13e1ba00f995c7e59a3
RamonDonadeu <43962562+RamonDonadeu@users.noreply.github.com>
2022-12-02 08:31:17 +0100

    Day 1

---
+++ 2022/Day1/main.py
@@ -0,0 +1,51 @@
+# Using readlines()
+file = open('AdventCalendar/Day1/data.txt', 'r')
+Lines = file.readlines()
+
+elve = 1
+maxCalories=0
+countCalories=0
+countElve = 1
+
+# Part 1
+
+for (line) in Lines:
+    if(line=='\n'):
+        if(countCalories > maxCalories):
+            maxCalories = countCalories
+            elve = countElve        
+        countElve += 1
+        countCalories = 0
+    else:
+        countCalories+= int(line)
+
+print("Elve " + str(elve) + " has " + str(maxCalories) + " Calories Stored")
+
+# Part 2
+
+maxCalories= [0,0,0]
+countCalories= 0
+countElve = 1
+
+# Part 1
+
+for (line) in Lines:
+    if(line=='\n'):
+        lower = 0
+        for i in range(len(maxCalories)):
+            if(maxCalories[i] < maxCalories[lower]):
+                lower = i 
+        if(maxCalories[lower] < countCalories):
+            maxCalories[lower] = countCalories 
+        countCalories = 0
+    else:
+        countCalories+= int(line)
+
+lower = 0
+for i in range(len(maxCalories)):
+    if(maxCalories[i] < maxCalories[lower]):
+        lower = i 
+if(maxCalories[lower] < countCalories):
+    maxCalories[lower] = countCalories 
+countCalories = 0
+print("Top 3 elves have " + str(sum(maxCalories)) + " Calories Stored")
