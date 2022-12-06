87c24a1c0bd618b3f02e2df2230146a63e9f15e3
RamonDonadeu <ramondonadeucaballero@gmail.com>
2022-12-06 12:36:31 +0100

    Day 6

--- 2022/Day6/main.py
+++ 2022/Day6/main.py
@@ -3,24 +3,39 @@ file = (os.path.dirname(os.path.realpath(__file__))) + "\data.txt"
 file = open(file, 'r')
 Lines = file.readlines()
 
+def check(string):
+    for char in string:
+        sepString = string.split(char)
+        if(len(sepString)>2):
+            return False
+    return True
+
+checker = ''
 counter = 0
 for line in Lines:
-    line = line.split('\n')[0].split(',')
-    if((int((line[0].split('-')[0]))<=int(line[1].split('-')[0]) and int(line[0].split('-')[1])>=int(line[1].split('-')[1])) or (int(line[1].split('-')[0])<=int((line[0].split('-')[0])) and int(line[1].split('-')[1])>=int(line[0].split('-')[1]))):
+    for char in line:
+        if(len(checker)==4):
+            checker = checker[1:]
+        checker=checker+char
         counter += 1
+        if(len(checker)==4):
+            if(check(checker)):
+                break
 
-print(str(counter) + " group of tasks are overlapped")
+print('first marker after character '+str(counter))
 
 # Part 2
 
+checker = ''
 counter = 0
 for line in Lines:
-    line = line.split('\n')[0].split(',')
-    if( (int(line[0].split('-')[0]) >= int(line[1].split('-')[0]) and int(line[0].split('-')[0]) <= int(line[1].split('-')[1])) or
-        (int(line[0].split('-')[1]) >= int(line[1].split('-')[0]) and int(line[0].split('-')[1]) <= int(line[1].split('-')[1])) or
-        (int(line[1].split('-')[0]) >= int(line[0].split('-')[0]) and int(line[1].split('-')[0]) <= int(line[0].split('-')[1])) or
-        (int(line[1].split('-')[1]) >= int(line[0].split('-')[0]) and int(line[1].split('-')[1]) <= int(line[0].split('-')[1]))
-        ):
+    for char in line:
+        if(len(checker)==14):
+            checker = checker[1:]
+        checker=checker+char
         counter += 1
+        if(len(checker)==14):
+            if(check(checker)):
+                break
 
-print(str(counter) + " tasks are overlapped")
+print('first marker after character '+str(counter))
