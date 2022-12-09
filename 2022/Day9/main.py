fadfa8de77eb4be90337c69a256f40e99ce7d09f
Ramon Donadeu Caballero <ramondonadeucaballero@gmail.com>
2022-12-09 20:14:10 +0100

    Day 9

--- 2022/Day9/main.py
+++ 2022/Day9/main.py
@@ -3,68 +3,96 @@ file = (os.path.dirname(os.path.realpath(__file__))) + "\data.txt"
 file = open(file, 'r')
 Lines = file.readlines()
 
-result  = 0
-directories = {}
+headPosition = {'x' : 50,'y': 50}
+tailPosition = {'x': 50,'y': 50}
+positions = set([])
 
-def createStructure():   
-    global result 
-    global directories
-    thisLevel = {}
-    line = Lines.pop(0)
-    while len(Lines) > 0:
-        if(line[0] == '$'):
-            if(line[2:4] == 'ls'):
-                line = Lines.pop(0)
-                while line[0] != '$':
-                    if(line[0:3] != 'dir'):
-                       thisLevel[line.split(' ')[1]] = line.split(' ')[0]
-                    if(len(Lines)>0):
-                        line = Lines.pop(0)   
-                    else: 
-                        size = 0
-                        for sizes in thisLevel:
-                            size += int(thisLevel[sizes])
-                        return size
-            if(line[2:4] == 'cd'):
-                if(line[5:7] != '..'):          
-                    size = createStructure()  
-                    if(size < 100000):
-                        result += size
-                    
-                    directories[line[5:7]] = size
-                    thisLevel[line.split(' ')[2]] = size
-                    if(len(Lines)>0):
-                        line = Lines.pop(0)   
-                    else: 
-                        size = 0
-                        for sizes in thisLevel:
-                            size += int(thisLevel[sizes])
-                        return size
-                else:
-                    size = 0
-                    for sizes in thisLevel:
-                        size += int(thisLevel[sizes])
-                    return size
+for line in Lines:
+    line = line.split('\n')[0]
+    direction = line[0]
+    moves = int(line.split(' ')[1])
+    for move in range(moves):
+        if direction == 'R':
+            headPosition['x'] += 1
+        elif direction == 'L':
+            headPosition['x']-=1
+        elif direction == 'U':
+            headPosition['y']-=1
+        else:
+            headPosition['y'] += 1
+        
+        if headPosition['x'] - tailPosition['x'] == -2:
+            tailPosition['x']-=1
+            if headPosition['y'] - tailPosition['y'] == -1:
+                tailPosition['y']-=1
+            if headPosition['y'] - tailPosition['y'] == 1:
+                tailPosition['y']+=1
+        if headPosition['x'] - tailPosition['x'] == 2:
+            tailPosition['x']+=1
+            if headPosition['y'] - tailPosition['y'] == 1:
+                tailPosition['y']+=1
+            if headPosition['y'] - tailPosition['y'] == -1:
+                tailPosition['y']-=1
+        if headPosition['y'] - tailPosition['y'] == -2:
+            tailPosition['y']-=1
+            if headPosition['x'] - tailPosition['x'] == -1:
+                tailPosition['x']-=1
+            if headPosition['x'] - tailPosition['x'] == 1:
+                tailPosition['x']+=1
+        if headPosition['y'] - tailPosition['y'] == 2:
+            tailPosition['y']+=1
+            if headPosition['x'] - tailPosition['x'] == 1:
+                tailPosition['x']+=1
+            if headPosition['x'] - tailPosition['x'] == -1:
+                tailPosition['x']-=1
 
-result = 0  
-Lines.pop(0)
-total = createStructure()
+        positions.add(str(tailPosition['x'])+'X'+str(tailPosition['y'])+'Y')
 
-print('Freeable space is: ' + str(result))
+print('The second knot visited ' + str(len(positions)) + ' positions')
 
-print('The total size of the outermost directory is ' + str(total))
-unused = 70000000-total
-print('This means that the size of the unused space must currently be ' + str(unused))
-print('We need 30000000 of free space to update the system')
-needed = 30000000-(70000000-total)
-print('Therefore, the update still requires a directory with total size of at least ' + str(needed))
+ropePosition = [{'x' : 50,'y': 50},{'x': 50,'y': 50},{'x': 50,'y': 50},{'x': 50,'y': 50},{'x': 50,'y': 50},{'x': 50,'y': 50},{'x': 50,'y': 50},{'x': 50,'y': 50},{'x': 50,'y': 50},{'x': 50,'y': 50}]
+positions = set([])
 
-bestSpace = -1
-best = ''
-for dir in directories:
-    if(directories[dir]>needed):
-        if(bestSpace>directories[dir]-needed or bestSpace == -1):
-            bestSpace=directories[dir]-needed
-            best = dir
+for line in Lines:
+    line = line.split('\n')[0]
+    direction = line[0]
+    moves = int(line.split(' ')[1])
+    for move in range(moves):
+        if direction == 'R':
+            ropePosition[0]['x'] += 1
+        elif direction == 'L':
+            ropePosition[0]['x']-=1
+        elif direction == 'U':
+            ropePosition[0]['y']-=1
+        else:
+            ropePosition[0]['y'] += 1
+        for knot in range(1,len(ropePosition)):
+            if ropePosition[knot-1]['x'] - ropePosition[knot]['x'] == -2:
+                ropePosition[knot]['x']-=1
+                if ropePosition[knot-1]['y'] - ropePosition[knot]['y'] <= -1:
+                    ropePosition[knot]['y']-=1
+                if ropePosition[knot-1]['y'] - ropePosition[knot]['y'] >= 1:
+                    ropePosition[knot]['y']+=1
+            if ropePosition[knot-1]['x'] - ropePosition[knot]['x'] == 2:
+                ropePosition[knot]['x']+=1
+                if ropePosition[knot-1]['y'] - ropePosition[knot]['y'] >= 1:
+                    ropePosition[knot]['y']+=1
+                if ropePosition[knot-1]['y'] - ropePosition[knot]['y'] <= -1:
+                    ropePosition[knot]['y']-=1
+            if ropePosition[knot-1]['y'] - ropePosition[knot]['y'] == -2:
+                ropePosition[knot]['y']-=1
+                if ropePosition[knot-1]['x'] - ropePosition[knot]['x'] <= -1:
+                    ropePosition[knot]['x']-=1
+                if ropePosition[knot-1]['x'] - ropePosition[knot]['x'] >= 1:
+                    ropePosition[knot]['x']+=1
+            if ropePosition[knot-1]['y'] - ropePosition[knot]['y'] == 2:
+                ropePosition[knot]['y']+=1
+                if ropePosition[knot-1]['x'] - ropePosition[knot]['x'] >= 1:
+                    ropePosition[knot]['x']+=1
+                if ropePosition[knot-1]['x'] - ropePosition[knot]['x'] <= -1:
+                    ropePosition[knot]['x']-=1
 
-print('The smallest directory to delete is ' + str(directories[best]))
+        positions.add(str(ropePosition[9]['x'])+'X'+str(ropePosition[9]['y'])+'Y')
+
+        
+print('The last knot visited ' + str(len(positions)) + ' positions')
