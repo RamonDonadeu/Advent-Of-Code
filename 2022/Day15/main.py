59cb8a38801bcf83d8a4a6283c30c0b2f534aa9b
RamonDonadeu <ramondonadeucaballero@gmail.com>
2022-12-21 18:48:53 +0100

    Day15 Not Wokinrg part 2

--- 2022/Day15/main.py
+++ 2022/Day15/main.py
@@ -1,7 +1,7 @@
 import os 
 import time
 import math
-file = (os.path.dirname(os.path.realpath(__file__))) + "/data.txt"
+file = (os.path.dirname(os.path.realpath(__file__))) + "\data.txt"
 file = open(file, 'r')
 Lines = file.readlines()
 
@@ -44,15 +44,64 @@ for line in Lines:
     if(mapCoords['top'] == '' or coord['beacon']['y'] < mapCoords['top'] ):
         mapCoords['top']=coord['beacon']['y']
 
-checkRow = 2000000
+checkRow = 10
+topRight = set()
+topLeft =set()
+botRight = set()
+botLeft = set()
+
 for sensor in sensors:
     distance = sensor['distance']
     farAway = distance - abs(sensor['sensor']['y']-checkRow)
     for column in range(sensor['sensor']['x'] - farAway, sensor['sensor']['x']+farAway+1):
         if(not positionOcupied({'x':column,'y':checkRow})):
             impossible.add(column)
-    
+
+    topPosition = sensor['sensor']['y'] - distance
+    botPosition = sensor['sensor']['y'] + distance
+    leftPosition = sensor['sensor']['x'] - distance
+    rightPosition = sensor['sensor']['x'] + distance
+    sensor = sensor['sensor']
+    if(topPosition >= checkRow):
+        topLeft.add(sensor['x'] + abs(checkRow-topPosition))
+        topRight.add(sensor['x'] - abs(checkRow-topPosition))
+        botLeft.add(sensor['x'] - abs(checkRow-sensor['y'])-distance)
+        botRight.add(sensor['x'] + abs(checkRow-sensor['y'])+distance)
+    elif(topPosition <= checkRow): 
+        topLeft.add(sensor['x'] - abs(checkRow-topPosition))
+        topRight.add(sensor['x'] + abs(checkRow-topPosition))
+        if(sensor['y'] <= checkRow):
+            botLeft.add(sensor['x'] + abs(checkRow-sensor['y'])-distance)
+            botRight.add(sensor['x'] - abs(checkRow-sensor['y'])+distance)
+        elif(sensor['y'] >= checkRow):
+            botLeft.add(sensor['x'] + abs(checkRow-sensor['y'])-distance)
+            botLeft.add(sensor['x'] - abs(checkRow-sensor['y'])+distance)
+
 print(len(impossible))
 
+toLeft=[]
+toRight=[]
 
-        
+for points in topLeft:
+    for pointsPlus in botRight:
+        if(points == pointsPlus - 2):
+            toLeft.append({'br':[points,checkRow],'tl':[pointsPlus,checkRow]})
+
+for points in botLeft:
+    for pointsPlus in topRight:
+        if(points == pointsPlus + 2):
+            toRight.append(({'bl':[points-2,checkRow-2],'tr':[pointsPlus,checkRow]}))
+
+found = False
+for group in toLeft:
+    for x in range(0,checkRow+1):
+        newGroup = {'br':[group['br'][0]+x,group['br'][1]-x]}
+        for perpendicular in toRight:
+            newPerp = {'bl':[perpendicular['bl'][0]-x,perpendicular['bl'][1]+x]}
+            if(newPerp['bl'][1]+1 == newGroup['br'][1]+x and newPerp['bl'][0] == group['br'][0]+1):
+                found = True
+            if(newPerp['bl'][1]+1 == newGroup['br'][1]-x and newPerp['bl'][0] == group['br'][0]+1):
+                found = True
+        if(found):
+            break
+            
